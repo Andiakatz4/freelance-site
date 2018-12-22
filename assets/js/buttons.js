0c34e070c0c543a1e74a32e2c8e5f2f9d700ba22
Andiakatz4 <andiakatz@gmail.com>
2018-12-21 17:35:37 -0800

    changed Andia Katz Web Design logo from photo to h1 text and styled

---
+++ assets/js/buttons.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#about_button").hover(function(){
+      $(this).css("border-color", "#103c54");
+      }, function(){
+      $(this).css("border-color", "#103c54");
+    });
+  });
